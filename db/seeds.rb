9e318e9ba3122032a2d23ec00ebcd5f21aedb4e1
sarita2129 <nairsarita2129@gmail.com>
2019-01-21 11:59:52 +1100

    seed commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,10 @@
 # dob:text image:text address:text phone:text companyname:text companyaddress:text
 # companywebsite:text companycontactinfo:text
 # Work.update :user_id => 6
+# User.destroy.all
+# u1 = User.create :email => 'test4@ga.co', :password => 'chicken', :admin => true, :image => ''
+# u2 = User.create :email => 'test5@ga.co', :password => 'chicken'
+u1 = User.find_by :email => 'test4@ga.co'
+w1 = Work.create :title => 'BLOSSOM JERSEY DRESS',:image => 'http://www.bodenimages.com/productimages/zoom/18waut_j0233_nav_w02.jpg', :description => "There's no denying it: every woman needs a statement dress. Behold this scattered-flower print – designed in-house, of course – crafted from a drapey fabric so comfortable you'll never want to take it off. We've added fluted sleeves and a full skirt for an elegant touch, while subtle gathers at the front and back give it that figure-skimming WOW factor."
 
-u1 = User.create :email => 'test4@ga.co', :password => 'chicken', :admin => true, :image => ''
-u2 = User.create :email => 'test5@ga.co', :password => 'chicken'
+u1.works << w1
