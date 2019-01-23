34d688d31f745734857cde79a396ea24ba92880b
sarita2129 <nairsarita2129@gmail.com>
2019-01-24 10:35:45 +1100

    commented jquery

--- app/assets/javascripts/gostyle.js
+++ app/assets/javascripts/gostyle.js
@@ -1,42 +1,42 @@
-$(document).ready(function(){
-  // $('.astar').click(function(e){
-  //   // e.preventDefault;
-  //   alert($(this).attr("name"));
-  //
-  // });
-  // alert($("#valdisable").val());
-  // $('#drpseller').addClass("drp");
-  $('a.astar').on('click', function(e){
-    e.preventDefault();
-    console.log($(this).parent());
-    console.log($(this).prevUntil("p"));
-    const prevstar = $(this).prevUntil("p");
-    // const star = Number($(this).attr("name"));
-    // for(const i = 1; i <= star; i++)
-    // {
-    //   starselect
-    //
-    // }
-    // prevstar.$each()
-    $.each(prevstar, function(key) {
-    $key.addClass("starselect");
-  });
-});
-$('#navbarDropdownMenuLink').on('click', function(){
-    $('.btn-navbar').click(); //bootstrap 2.x
-    $('.navbar-toggle').click(); //bootstrap 3.x by Richard
-    $('.navbar-toggler').click(); //bootstrap 4.x
-});
-$("a.astar").on("ajax:success", function(event, xhr, status, error){
-  // [data, status, xhr] = event.detail
-  $("#new_article").append(xhr.responseText);
-}).on("ajax:error", function(event){
-  $("#new_article").append("<p>ERROR</p>");
-});
-
-$("btncontract").click(function(){
-  alert()
-});
-
-
-});
+// $(document).ready(function(){
+//   // $('.astar').click(function(e){
+//   //   // e.preventDefault;
+//   //   alert($(this).attr("name"));
+//   //
+//   // });
+//   // alert($("#valdisable").val());
+//   // $('#drpseller').addClass("drp");
+//   $('a.astar').on('click', function(e){
+//     e.preventDefault();
+//     console.log($(this).parent());
+//     console.log($(this).prevUntil("p"));
+//     const prevstar = $(this).prevUntil("p");
+//     // const star = Number($(this).attr("name"));
+//     // for(const i = 1; i <= star; i++)
+//     // {
+//     //   starselect
+//     //
+//     // }
+//     // prevstar.$each()
+//     $.each(prevstar, function(key) {
+//     $key.addClass("starselect");
+//   });
+// });
+// $('#navbarDropdownMenuLink').on('click', function(){
+//     $('.btn-navbar').click(); //bootstrap 2.x
+//     $('.navbar-toggle').click(); //bootstrap 3.x by Richard
+//     $('.navbar-toggler').click(); //bootstrap 4.x
+// });
+// $("a.astar").on("ajax:success", function(event, xhr, status, error){
+//   // [data, status, xhr] = event.detail
+//   $("#new_article").append(xhr.responseText);
+// }).on("ajax:error", function(event){
+//   $("#new_article").append("<p>ERROR</p>");
+// });
+//
+// $("btncontract").click(function(){
+//   alert()
+// });
+//
+//
+// });
